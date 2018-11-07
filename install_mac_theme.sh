9d8390f85777d1b18471ca234c577d3c21872102
fourseaforfriend <2015903671@qq.com>
2018-11-07 19:13:47 +0800

    Add files via upload

---
+++ install_mac_theme.sh
@@ -0,0 +1,11 @@
+# https://www.linuxidc.com/Linux/2017-04/142631.htm 
+sudo add-apt-repository ppa:noobslab/macbuntu
+sudo apt-get update
+sudo apt-get install macbuntu-os-icons-lts-v7
+sudo apt-get install macbuntu-os-ithemes-lts-v7
+
+sudo apt-get install unity-tweak-tool
+
+sudo apt-get install docky
+
+sudo apt-get install slingscold
