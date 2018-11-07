bb1682c4168c70c50e9a6d84d0713a23175632cb
fourseaforfriend <2015903671@qq.com>
2018-11-07 19:14:06 +0800

    Add files via upload

---
+++ install_theme.sh
@@ -0,0 +1,8 @@
+# https://www.jianshu.com/p/246d629f1ee5
+sudo apt-cache search unity-tweak-tool
+sudo apt-get install -y unity-tweak-tool
+sudo add-apt-repository ppa:noobslab/themes
+sudo apt-get update
+sudo apt-get install mac-ithemes-v3
+sudo apt-get install mac-icons-v3
+
